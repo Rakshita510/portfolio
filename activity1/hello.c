96e1fc6027e124eda4d0eeaf702b339f6a2c246a
Sheetal532630 <csheetall300@gmail.com>
2026-09-03 08:53:37 +0530

    updated

--- activity1/hello.c
+++ activity1/hello.c
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 int main() {
     printf("Hello, World!\n");
     return 0;
-=======
-#include <stdio.h>
-
-int main() {
-    printf("Hello, World!\n");
-    return 0;
->>>>>>> 68ec74fae5443e5c7ccfc0d6ae995f0f8ab8fede
 }
